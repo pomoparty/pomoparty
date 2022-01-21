3f4c72a8f0f93f3308b7882b0892a5752d5e828d
starfy84 <dereck.tu@hotmail.com>
2022-01-20 21:20:17 -0500

    Add main box, implement simple timer w/start & stop

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBoxComponent } from './main-box/main-box.component';
+import { SliderComponent } from './main-box/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBoxComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/main-box/main-box.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+
+@Component({
+  selector: 'app-main-box',
+  templateUrl: './main-box.component.html',
+  styleUrls: ['./main-box.component.scss']
+})
+export class MainBoxComponent implements OnInit {
+
+  endTime: number = 0;
+  interval: number = 25000;
+  isPaused: boolean = false;
+
+  timerSub: Subscription;
+  timeLeft: number;
+
+  pauseTime: number;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.timerSub = timer(0, 1000).subscribe(() => {
+      if (!this.isPaused) this.updateTimeLeft()
+    });
+  }
+
+  ngOnDestroy(): void{
+    this.timerSub.unsubscribe();
+    this.timerSub = null;
+  }
+
+  startTimer(){
+    if(this.isPaused){
+      this.endTime += Date.now() - this.pauseTime;
+      this.isPaused = false;
+    }else{
+      this.endTime = Date.now() + this.interval;
+    }
+  }
+
+  stopTimer(){
+    if (!this.isPaused && this.endTime - Date.now() >= 0){
+      this.isPaused = true;
+      this.pauseTime = Date.now();
+    }
+  }
+
+  updateTimeLeft(){
+    const temp: number = Math.round((this.endTime - Date.now())/1000);
+
+    this.timeLeft = (temp <= 0) ? 25 : temp; 
+  }
+
+}
