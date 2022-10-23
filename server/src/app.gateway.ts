01b1e753908883dd7df5906eeb9dad0564800786
Dereck Tu <dereck.tu@hotmail.com>
2022-10-23 02:00:46 -0400

    test

---
+++ server/src/app.gateway.ts
@@ -0,0 +1,24 @@
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WsResponse } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+@WebSocketGateway()
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  
+  private logger: Logger = new Logger('AppGateway'); 
+  
+  afterInit(server: Server) {
+    this.logger.log('Initialized');
+  }
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+  
+
+  @SubscribeMessage('startTimer')
+  handleMessage(client: Socket, payload: any): WsResponse<string> {
+    return {event: 'startTimer', data:'Hello world!'};
+  }
+}
