68727642925c2a2cb6010d27539ddb2a49764b0a
Dereck Tu <dereck.tu@hotmail.com>
2022-10-23 03:48:00 -0400

    Attempt to connect websocket

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { WebsocketService } from "./websocket.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WebsocketService],
 })
 export class AppComponent {
   title = 'pomoparty';
+
+  constructor(private SocketService: WebsocketService){} 
+
 }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  socketUrl: '',
+  socketUrl: 'ws://localhost:3000',
 };
 
 /*
