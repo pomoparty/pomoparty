9a156993916694f3d6b7df832b9ed55face08a60
Dereck Tu <dereck.tu@hotmail.com>
2022-10-23 03:57:23 -0400

    Update to angular 11

--- client/src/app/main-box/main-box.component.spec.ts
+++ client/src/app/main-box/main-box.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MainBoxComponent } from './main-box.component';
 
@@ -6,7 +6,7 @@ describe('MainBoxComponent', () => {
   let component: MainBoxComponent;
   let fixture: ComponentFixture<MainBoxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MainBoxComponent ]
     })
