728efea4f12338d233a13da821788a5efeacc108
Dereck Tu <dereck.tu@hotmail.com>
2022-10-23 20:04:27 -0400

    Send timer update from BE

--- server/src/app.gateway.ts
+++ server/src/app.gateway.ts
@@ -1,16 +1,30 @@
-import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WsResponse, WebSocketServer } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WsResponse,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
+
 @WebSocketGateway({
   cors: true,
 })
-export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  
-  private logger: Logger = new Logger('AppGateway'); 
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger: Logger = new Logger('AppGateway');
+
+  private activeTimers: {
+    [key: string]: NodeJS.Timer;
+  } = {};
 
   @WebSocketServer()
   private server: Server;
-  
+
   afterInit(server: Server) {
     this.logger.log('Initialized');
   }
@@ -20,10 +34,42 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
   handleDisconnect(client: Socket) {
     this.logger.log(`Client disconnected: ${client.id}`);
   }
-  
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string): WsResponse<string> {
+    client.join(room);
+    return { event: 'joinRoom', data: room };
+  }
 
   @SubscribeMessage('startTimer')
-  handleMessage(client: Socket, payload: any): WsResponse<string> {
-    return {event: 'startTimer', data:'Hello world!'};
+  handleMessage(client: Socket, durationMillis: number) {
+    this.logger.log('startTimer Received');
+    if (client.rooms.size != 2) {
+      client.emit('error', 'rooms');
+      this.logger.log(
+        `${client.id} must be in exactly one room excluding itself`,
+      );
+      return;
+    }
+    const startTimeMillis: number = Date.now();
+
+    // Cast number to number because postman still sends payload as string
+    const endTimeMillis: number = startTimeMillis + Number(durationMillis);
+    for (let room of client.rooms) {
+      if (room == client.id) continue;
+      this.logger.log(room);
+      this.server.to(room).emit('startTimer', endTimeMillis);
+      this.logger.log(
+        `${client.id} has started the timer in ${room} for ${durationMillis} milliseconds`,
+      );
+      let interval = setInterval(() => {
+        this.server.to(room).emit('updateTimer', endTimeMillis - Date.now());
+        if (endTimeMillis - Date.now() <= 0) {
+          this.logger.log('timer stopped');
+          clearInterval(this.activeTimers[room]);
+        }
+      }, 1000); // tick every second
+      this.activeTimers[room] = interval;
+    }
   }
 }
